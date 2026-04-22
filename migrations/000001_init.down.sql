91cb503e7eb39a471734850698b9cd004accedaf
dangngockhieu <dangngockhieu28082005@gmail.com>
2026-04-22 09:37:43 +0700

    update migrations

--- migrations/000001_init.down.sql
+++ migrations/000001_init.down.sql
@@ -1,6 +1,8 @@
-DROP TABLE IF EXISTS vehicle_logs;
 DROP TABLE IF EXISTS slot_histories;
+DROP TABLE IF EXISTS parking_sessions;
 DROP TABLE IF EXISTS refresh_tokens;
+DROP TABLE IF EXISTS gates;
+DROP TABLE IF EXISTS rfid_cards;
 DROP TABLE IF EXISTS parking_slots;
 DROP TABLE IF EXISTS iot_devices;
 DROP TABLE IF EXISTS parking_lots;
