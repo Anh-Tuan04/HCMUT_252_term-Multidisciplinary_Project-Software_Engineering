a94e70008317be6c0e6e65cb4fd29551ddda0d5e
Anh-Tuan04 <tuan.vuatuan2k4@hcmut.edu.vn>
2026-04-29 09:07:23 +0700

    "Change vite.config.js to run in 5173 port instead through Caddy"

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Ép Vite chạy ở cổng 5173 để Caddy biết đường mà tìm
-    port: 5173,
-    strictPort: true,
-    // Cấu hình HMR để nó chạy qua cổng 443 của Caddy thay vì chạy trực tiếp
-    hmr: {
-      protocol: 'ws',
-      host: 'localhost',
-    },
-  },
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     // Ép Vite chạy ở cổng 5173 để Caddy biết đường mà tìm
+//     port: 5173,
+//     strictPort: true,
+//     // Cấu hình HMR để nó chạy qua cổng 443 của Caddy thay vì chạy trực tiếp
+//     hmr: {
+//       protocol: 'ws',
+//       host: 'localhost',
+//     },
+//   },
+// })
